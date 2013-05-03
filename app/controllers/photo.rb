553b228eacea56742cff0abd6829c16ac18f1973
Angie Bui <angiebui@gmail.com>
2013-05-02 23:52:17 -0700

    add to sets

--- app/controllers/photo.rb
+++ app/controllers/photo.rb
@@ -1,15 +1,21 @@
 get '/upload' do
-  erb :upload
+	unless current_user
+	@errors = "Please login or signup to load"
+  end
+
+ erb :upload
 end
 
 post "/upload" do
   if current_user 
-    photo = Photo.new
+  	album = current_user.albums.find_or_create_by_title(params[:title])
+    photo = album.photos.new
     photo.filename.store!(params[:filename])
     photo.save
-    redirect to('/') # redirect to that album?
+    redirect to("/album/#{album.id}")
   else
-    @errors = "Please login to upload"
+    @errors = "You must login or signup to upload"
+    erb :upload
   end
 end
 
@@ -21,8 +27,19 @@ end
 
 get '/album/:id' do
 	@album = Album.find(params[:id])
+	@photos = @album.photos.order("created_at DESC")
 	erb :photo
 end
 
 get 'album/:id/:photo_id' do
+
 end
+
+post '/album/:id' do
+	photo = Album.find(params[:id]).photos.new
+	photo.filename.store!(params[:filename])
+	photo.save
+	
+	redirect to("/album/#{params[:id]}") #optimize so that it lands in that image specifically in album?
+end
+
